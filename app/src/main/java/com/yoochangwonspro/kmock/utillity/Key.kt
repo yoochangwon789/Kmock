9b68ce936441cf938a43b09be7f6fcce225e80e2
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-03-24 23:30:01 +0900

    Feat : Url, Key object 추가

---
+++ app/src/main/java/com/yoochangwonspro/kmock/utillity/Key.kt
@@ -0,0 +1,5 @@
+package com.yoochangwonspro.kmock.utillity
+
+object Key {
+    const val SERVICE_KEY = "WKZHSkx8qDVMqv%2FpDIMoMnghtdYfEZbPepkFPzwUEhJDLFuv8RGs5gXcZrZbU3Fx04C3QqDMVGv2Je6hjWGjZg%3D%3D"
+}
---
+++ app/src/main/java/com/yoochangwonspro/kmock/utillity/Url.kt
@@ -0,0 +1,5 @@
+package com.yoochangwonspro.kmock.utillity
+
+object Url {
+    const val BASE_URL = "http://apis.data.go.kr/B552881/kmooc/courseList"
+}
