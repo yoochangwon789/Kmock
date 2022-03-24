a683a41df5886f41a68c9d45416d0a83a371fdaf
yoochangwon789 <59136156+yoochangwon789@users.noreply.github.com>
2022-03-25 00:19:24 +0900

    Feat : DefaultLectureRepository 의 getLectureList 함수 구현

--- app/src/main/java/com/yoochangwonspro/kmock/data/repository/DefaultLectureRepository.kt
+++ app/src/main/java/com/yoochangwonspro/kmock/data/repository/DefaultLectureRepository.kt
@@ -1,14 +1,22 @@
 package com.yoochangwonspro.kmock.data.repository
 
 import com.yoochangwonspro.kmock.data.model.Lecture
+import com.yoochangwonspro.kmock.data.network.LectureService
 import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 
 class DefaultLectureRepository(
-    private val lectureApi: LectureRepository,
+    private val lectureApi: LectureService,
     private val ioDispatcher: CoroutineDispatcher
 ) : LectureRepository {
 
-    override suspend fun getLectureList(): List<Lecture> {
-        TODO("Not yet implemented")
+    override suspend fun getLectureList(): List<Lecture> = withContext(ioDispatcher) {
+        val response = lectureApi.getLectures()
+
+        return@withContext if (response.isSuccessful) {
+            response.body()?.results ?: listOf()
+        } else {
+            listOf()
+        }
     }
 }
